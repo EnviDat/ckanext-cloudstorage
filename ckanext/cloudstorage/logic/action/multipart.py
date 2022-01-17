a4628abd7da4cb223adbfbc953ed60d321a8c53f
sam <sam.woodcock@protonmail.com>
2022-01-17 11:54:32 +0000

    s3 multipart list_parts change key variable name

--- ckanext/cloudstorage/logic/action/multipart.py
+++ ckanext/cloudstorage/logic/action/multipart.py
@@ -326,7 +326,7 @@ def multipart_list_parts(context, data_dict):
     try:
         upload_id = toolkit.get_or_bust(data_dict, "uploadId")
 
-        if key := data_dict.get("key") is not None:
+        if key := data_dict.get("uploadKey") is not None:
             rid = None
             filename = None
         else:

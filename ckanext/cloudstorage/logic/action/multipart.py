81ce25823b5a68f55ca4b8d9ae702517303dbade
sam <sam.woodcock@protonmail.com>
2022-01-17 12:26:35 +0000

    multipart list_parts fix pop

--- ckanext/cloudstorage/logic/action/multipart.py
+++ ckanext/cloudstorage/logic/action/multipart.py
@@ -341,7 +341,8 @@ def multipart_list_parts(context, data_dict):
             upload_id, key=upload_key, rid=rid, filename=filename
         )
         # Instead of json encoding datetime, simply remove LastModified
-        multipart_parts = [part.pop('LastModified', None) for part in multipart_parts]
+        for part in multipart_parts:
+            part.pop('LastModified', None)
 
     except Exception as e:
         log.error(f"EXCEPTION multipart_list_parts: {e}")
